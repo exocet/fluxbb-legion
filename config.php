25b640457a13976f510a1c63667575c9caac402d
exocet <tecoxe@gmail.com>
2016-03-11 22:55:34 +0100

    Organisation des fichiers

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+$db_type = 'mysqli';
+$db_host = 'localhost';
+$db_name = 'fluxbb-legion';
+$db_username = 'wow';
+$db_password = 'wow';
+$db_prefix = '';
+$p_connect = false;
+
+$cookie_name = 'pun_cookie_1a2f6b';
+$cookie_domain = '';
+$cookie_path = '/';
+$cookie_secure = 0;
+$cookie_seed = '57d2ffb7f0362b05';
+
+define('PUN', 1);
+define('PUN_DEBUG', 1);
+define('PUN_SHOW_QUERIES', 1);
