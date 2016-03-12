c8043f207a2463e5b770d6f85a6aa71ab826eba1
exocet <tecoxe@gmail.com>
2016-03-12 02:10:03 +0100

    Gestion des sous forums et ajouts de catégories/forums

--- config.php
+++ config.php
@@ -2,7 +2,6 @@
 
 $db_type = 'mysqli';
 $db_host = 'localhost';
-<<<<<<< HEAD
 $db_name = 'fluxbb-legion';
 $db_username = 'wow';
 $db_password = 'wow';
@@ -18,18 +17,3 @@ $cookie_seed = '57d2ffb7f0362b05';
 define('PUN', 1);
 define('PUN_DEBUG', 1);
 define('PUN_SHOW_QUERIES', 1);
-=======
-$db_name = 'fluxbb';
-$db_username = 'root';
-$db_password = 'apolop09';
-$db_prefix = '';
-$p_connect = false;
-
-$cookie_name = 'pun_cookie_4f4874';
-$cookie_domain = '';
-$cookie_path = '/';
-$cookie_secure = 0;
-$cookie_seed = '751cbd07229501a0';
-
-define('PUN', 1);
->>>>>>> origin/master
---
+++ sql/Forums insert.sql
@@ -0,0 +1,60 @@
+La Légion
+	Invités	
+	Evénements Jeux de Rôles
+	Evénements Jeux de Figurines
+	Bafouilles
+	Petites annonces
+	Communications du Conseil
+
+Conseil
+	Archives du conseil
+	Participation aux évènements
+	Tâches en cours
+	Au sujet des membres
+	Officiel
+
+Les Jeux
+	Jeux de Rôles
+		7ème Mer, Bloodlust, Cats ! La Mascarade, C.O.P.S., Dark Earth, Deadlands, Donjons & Dragons, EXIL, Fading Suns, Fuzion, Galaxcity 8, Guildes, Hollow Earth Expedition, 
+		INS/MV, Livre des 5 Anneaux, Mahamoth, Marvel, Monde des Ténèbres, Nephilim, Plagues dK, Polaris, Shadowrun, Tigres Volants, Two Pieces, Warhammer JdRF & 40k, Warsaw, 
+		Wasteland, Yggdrasil, Z-Corp, One-Shot
+	Jeux de Figurines
+	Jeux de Cartes et Jeux de Plateau
+
+Les Abimes
+	Corbeille
+	Archives
+
+
+
+INSERT INTO `forums` (`id`, `forum_name`, `forum_desc`, `redirect_url`, `moderators`, `num_topics`, `num_posts`, `last_post`, `last_post_id`, `last_poster`, `sort_by`, `disp_position`, `cat_id`, `parent_forum_id`) VALUES
+(NULL, '7ème Mer', NULL, NULL, NULL, 0, 0, NULL, NULL, NULL, 0, 1, 1, 14),
+(NULL, 'Bloodlust', NULL, NULL, NULL, 0, 0, NULL, NULL, NULL, 0, 0, 1, 14),
+(NULL, 'Cats ! La Mascarade', NULL, NULL, NULL, 0, 0, NULL, NULL, NULL, 0, 2, 1, 14),
+(NULL, 'C.O.P.S', NULL, NULL, NULL, 0, 0, NULL, NULL, NULL, 0, 5, 1, 14),
+(NULL, 'Dark Earth', NULL, NULL, NULL, 0, 0, NULL, NULL, NULL, 0, 4, 1, 14),
+(NULL, 'Deadlands', NULL, NULL, NULL, 0, 0, NULL, NULL, NULL, 0, 3, 1, 14),,
+(NULL, 'Donjons & Dragons', NULL, NULL, NULL, 0, 0, NULL, NULL, NULL, 0, 0, 1, 14),
+(NULL, 'EXIL', NULL, NULL, NULL, 0, 0, NULL, NULL, NULL, 0, 1, 1, 14),
+(NULL, 'Fading Suns', NULL, NULL, NULL, 0, 0, NULL, NULL, NULL, 0, 3, 1, 14),
+(NULL, 'Fuzion', NULL, NULL, NULL, 0, 0, NULL, NULL, NULL, 0, 4, 1, 14),
+(NULL, 'Galaxcity 8', NULL, NULL, NULL, 0, 0, NULL, NULL, NULL, 0, 0, 1, 14),
+(NULL, 'Guildes', NULL, NULL, NULL, 0, 0, NULL, NULL, NULL, 0, 1, 1, 14),
+(NULL, 'Hollow Earth Expedition', NULL, NULL, NULL, 0, 0, NULL, NULL, NULL, 0, 2, 1, 14),
+(NULL, 'INS/MV', NULL, NULL, NULL, 0, 0, NULL, NULL, NULL, 0, 0, 1, 14),
+(NULL, 'Livre des 5 Anneaux', NULL, NULL, NULL, 0, 0, NULL, NULL, NULL, 0, 1, 1, 14),
+(NULL, 'Mahamoth', NULL, NULL, NULL, 0, 0, NULL, NULL, NULL, 0, 2, 1, 14),
+(NULL, 'Marvel', NULL, NULL, NULL, 0, 0, NULL, NULL, NULL, 0, 4, 1, 14),
+(NULL, 'Monde des Ténèbres', NULL, NULL, NULL, 0, 0, NULL, NULL, NULL, 0, 0, 1, 14),
+(NULL, 'Nephilim', NULL, NULL, NULL, 0, 0, NULL, NULL, NULL, 0, 1, 1, 14),
+(NULL, 'Plagues dK', NULL, NULL, NULL, 0, 0, NULL, NULL, NULL, 0, 2, 1, 14),
+(NULL, 'Polaris', NULL, NULL, NULL, 0, 0, NULL, NULL, NULL, 0, 0, 1, 14),
+(NULL, 'Shadowrun', NULL, NULL, NULL, 0, 0, NULL, NULL, NULL, 0, 1, 1, 14),
+(NULL, 'Tigres Volants', NULL, NULL, NULL, 0, 0, NULL, NULL, NULL, 0, 2, 1, 14),
+(NULL, 'Two Pieces', NULL, NULL, NULL, 0, 0, NULL, NULL, NULL, 0, 1, 1, 14),
+(NULL, 'Warhammer JdRF & 40k', NULL, NULL, NULL, 0, 0, NULL, NULL, NULL, 0, 2, 1, 14),
+(NULL, 'Warsaw', NULL, NULL, NULL, 0, 0, NULL, NULL, NULL, 0, 0, 1, 14),
+(NULL, 'Wasteland', NULL, NULL, NULL, 0, 0, NULL, NULL, NULL, 0, 1, 1, 14),
+(NULL, 'Yggdrasil', NULL, NULL, NULL, 0, 0, NULL, NULL, NULL, 0, 2, 1, 14),
+(NULL, 'Z-Corp', NULL, NULL, NULL, 0, 0, NULL, NULL, NULL, 0, 0, 1, 14),
+(NULL, 'One-Shot', NULL, NULL, NULL, 0, 0, NULL, NULL, NULL, 0, 1, 1, 14);
